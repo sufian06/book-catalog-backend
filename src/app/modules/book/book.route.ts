032a799e1f7274cf18f551eba269b01259286ea1
Abu sufian <75737338+sufian06@users.noreply.github.com>
2023-07-14 21:55:56 +0600

    book route completed

--- src/app/modules/book/book.route.ts
+++ src/app/modules/book/book.route.ts
@@ -6,4 +6,8 @@ const router = express.Router();
 router.get('/', BookController.getAllBooks);
 router.post('/add-new-book', BookController.addNewBook);
 
+router.get('/:id', BookController.getSingleBook);
+router.patch('/:id', BookController.updateBook);
+router.delete('/:id', BookController.deleteBook);
+
 export const BookRoutes = router;
