afd4fb4219b3846e6a16dfa57f6b3cf4dd9b67f6
Abu sufian <75737338+sufian06@users.noreply.github.com>
2023-07-14 19:34:14 +0600

    create user controller

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { UserService } from './user.service';
+
+const createUser = catchAsync(async (req: Request, res: Response) => {
+  const { ...userData } = req.body;
+
+  const result = await UserService.createUser(userData);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User created successfully',
+    data: result,
+  });
+});
+
+export const UserController = {
+  createUser,
+};
